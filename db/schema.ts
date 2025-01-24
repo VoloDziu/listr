e664dd9fc54c72483b52ac2ac0c16638c2a65616
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-01-23 21:26:53 -0500

    schema for todos

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,24 @@
+import { relations } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const listsTable = sqliteTable("lists_table", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
 });
+
+export const listsRelations = relations(listsTable, ({ many }) => ({
+  todos: many(todosTable),
+}))
+
+export const todosTable = sqliteTable("todos_table", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  listId: int(),
+});
+
+export const todosRelations = relations(todosTable, ({ one }) => ({
+  list: one(listsTable, {
+    fields: [todosTable.listId],
+    references: [listsTable.id],
+  }),
+}));

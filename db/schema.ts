aa7fc4cc8a26545d5a53d07632e4127487478f1b
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-01-28 09:14:35 -0500

    archival schema changes

--- db/schema.ts
+++ db/schema.ts
@@ -1,20 +1,28 @@
-import { relations } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const listsTable = sqliteTable("lists_table", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
+  archivedAt: text()
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  parentList: int(),
 });
 
-export const listsRelations = relations(listsTable, ({ many }) => ({
+export const listsRelations = relations(listsTable, ({ many, one }) => ({
   todos: many(todosTable),
-}))
+  parentList: one(listsTable, {
+    fields: [listsTable.parentList],
+    references: [listsTable.id],
+  }),
+}));
 
 export const todosTable = sqliteTable("todos_table", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   listId: int().notNull(),
-  completed: int().default(0).notNull(),
+  completed: int({ mode: "boolean" }).default(false).notNull(),
 });
 
 export const todosRelations = relations(todosTable, ({ one }) => ({
